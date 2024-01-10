33f59b7a896b6b68a6d76f83fdaa229971b2da43
Udeet1107 <ugulati94@gmail.com>
2024-01-10 10:47:25 +0530

    Complete practice website code

---
+++ practice-project/src/ListElement.js
@@ -0,0 +1,15 @@
+
+
+function ListElement(props) {
+
+    return (
+    <li className='concept'>
+        <img src={props.concept.image} alt={props.concept.title} />
+        <h2>{props.concept.title}</h2>
+        <p>{props.concept.description}</p>
+    </li>
+    )
+
+}
+
+export default ListElement
